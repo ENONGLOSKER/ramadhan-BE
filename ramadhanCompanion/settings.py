ed0851f6bbdac5639020092790c4f99add2da432
ENONGLOSKER <ENONGLOSKER@gmail.com>
2025-03-05 08:03:26 +0700

    fix: time

--- ramadhanCompanion/settings.py
+++ ramadhanCompanion/settings.py
@@ -111,11 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Makassar'
+TIME_ZONE = 'Asia/Makassar'  # Zona waktu WITA
+USE_TZ = True  # Pastikan ini diatur ke True untuk menggunakan zona waktu yang benar
 
-USE_I18N = True
-
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
@@ -138,7 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://ramadhan-fe.vercel.app/",
+    "https://ramadhan-fe.vercel.app",
     "http://127.0.0.1:5500",
 ]
 CORS_ALLOW_METHODS = [
